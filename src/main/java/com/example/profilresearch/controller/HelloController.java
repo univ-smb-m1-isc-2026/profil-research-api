94e134c9e75bb92d38a8a2d1d5449f4632a8b683
Morgane <farez.morgane@gmail.com>
2026-03-06 14:44:24 +0100

    fix: change package import

--- src/main/java/com/example/profilresearch/controller/HelloController.java
+++ src/main/java/com/example/profilresearch/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.profilresearch;
+package com.example.profilresearch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
